6114100d7816a041b4dc9294504e0be8ca270677
Bun <krissyheishman@gmail.com>
2019-05-28 13:11:59 -0700

    !djmode & !swapcam added

--- integrations/obs/ctrl.py
+++ integrations/obs/ctrl.py
@@ -1,52 +1,112 @@
-from integrations.obs.websocket import obs
+try:
+    from integrations.obs.websocket import obs
+except ImportError:
+    pass
 from utils.logger import loggyballs as log
 
 # config ze bot!
 import conf
 twitch_bot = conf.twitch_instance
 
+# DEBUG temp for testing
+dj_cam_state = True
+
+dj_mode = False
+
+# disables switching to facecam until it's toggled back off - privacy stuff w/e
+@twitch_bot.command('djmode', module='Stream VFX', perm=6)
+async def faceless(message):
+
+    global dj_mode
+    
+    if message.author.mod or message.author.name == conf.streamer:
+        if dj_mode is False:
+            msg = "DJ Mode: ENGAGED. Adjusting command structure..."
+            dj_mode = True
+        elif dj_mode is True:
+            msg = "DJ Mode: DISENGAGED. Command structure returning to it's original state..."
+            dj_mode = False
+
+        await twitch_bot.say(message.channel, msg)
+
 
 # ANCHOR Utilities
 
 # disables switching to facecam until it's toggled back off - privacy stuff w/e
-@twitch_bot.command('faceless')
+@twitch_bot.command('faceless', module='Stream VFX', perm=6)
 async def faceless(message):
-    if message.author.mod or message.author.name == conf.streamer:
-        obs.face_cam_allowed = not obs.face_cam_allowed
-        if obs.face_cam_allowed:
-            obs.enableSource('CAM - Face', True, '[SC] Cams (with frame)')
-            msg = "facecam back online!"
-        else:
-            obs.enableSource('CAM - Face', False, '[SC] Cams (with frame)')
-            msg = "facecam offline for an minuto. brb!"
 
-        await twitch_bot.say(message.channel, msg)
+    if dj_mode is False:
+
+        if message.author.mod or message.author.name == conf.streamer:
+            obs.face_cam_allowed = not obs.face_cam_allowed
+            if obs.face_cam_allowed:
+                obs.enableSource('CAM - Face', True, '[SC] Cams (with frame)')
+                msg = "facecam back online!"
+            else:
+                obs.enableSource('CAM - Face', False, '[SC] Cams (with frame)')
+                msg = "facecam offline for an minuto. brb!"
+
+    else:
+        msg = f"@{message.author.name} we're currently in DJ mode. Scene switching is disabled."
+
+    await twitch_bot.say(message.channel, msg)
 
 
 # ANCHOR Source Control
 
 # Swaps to face cam (SUB-only)
-@twitch_bot.command('face')
+@twitch_bot.command('face', module='Stream VFX', perm=0)
 async def face(message):
 
-    if not obs.face_cam_allowed:
-        msg = f"@{message.author.name}, facecam is disabled for a min. BRB. <3"
-    elif message.author.subscriber or message.author.mod or message.author.name == conf.streamer:
-        obs.enableSource('CAM - Face', True, '[SC] Cams (with frame)')
-        msg = 'Face cam is back!'
+
+    if dj_mode is False:
+        
+        if not obs.face_cam_allowed:
+            msg = f"@{message.author.name}, facecam is disabled for a min. BRB. <3"
+
+        elif message.author.subscriber or message.author.mod or message.author.name == conf.streamer:
+            obs.enableSource('CAM - Face', True, '[SC] Cams (with frame)')
+            msg = 'Face cam is back!'
+
+        else:
+            msg = f"@{message.author.name}, {obs.no_permissions_msg}"
+
     else:
-        msg = f"@{message.author.name}, {obs.no_permissions_msg}"
+        msg = f"@{message.author.name} we're currently in DJ mode. Scene switching is disabled."
 
     await twitch_bot.say(message.channel,msg)
 
 
 # Swaps to kb cam (SUB-only)
-@twitch_bot.command('kb')
+@twitch_bot.command('kb', module='Stream VFX', perm=0)
 async def kb(message):
 
+    if dj_mode is False:
+        if message.author.subscriber or message.author.mod or message.author.name == conf.streamer:
+            obs.enableSource('CAM - Face', False, '[SC] Cams (with frame)')
+            msg = 'Clickity-clack time!!'
+
+        else:
+            msg = f"@{message.author.name}, {obs.no_permissions_msg}"
+
+    else:
+        msg = f"@{message.author.name} we're currently in DJ mode. Scene switching is disabled."
+
+    await twitch_bot.say(message.channel, msg)
+
+
+# Swaps to kb cam (SUB-only)
+@twitch_bot.command('swapcam', module='Stream VFX', perm=0)
+async def kb(message):
+
+    global dj_cam_state
+
     if message.author.subscriber or message.author.mod or message.author.name == conf.streamer:
-        obs.enableSource('CAM - Face', False, '[SC] Cams (with frame)')
-        msg = 'Clickity-clack time!!'
+        obs.enableSource('CAM - Desktop', dj_cam_state, 'DJ')
+        msg = f'Cam swapped!'
+        dj_cam_state = not dj_cam_state
+
     else:
         msg = f"@{message.author.name}, {obs.no_permissions_msg}"
 
@@ -56,18 +116,27 @@ async def kb(message):
 # ANCHOR Scene Control
 
 # switches scene to chatting
-@twitch_bot.command('chat')
+@twitch_bot.command('chat', module='Stream VFX', perm=0)
 async def chat(message):
-    obs.switchScene('Chatting')
-    msg = f"@{message.author.name} switched the scene to Chatting."
+    if dj_mode is False:
+        obs.switchScene('Chatting')
+        msg = f"@{message.author.name} switched the scene to Chatting."
+
+    else:
+        msg = f"@{message.author.name} we're currently in DJ mode. Scene switching is disabled."
     await twitch_bot.say(message.channel, msg)
 
 
 # switches scene to live coding
-@twitch_bot.command('code')
+@twitch_bot.command('code', module='Stream VFX', perm=0)
 async def code(message):
-    obs.switchScene('Live Coding')
-    msg = f"@{message.author.name} switched the scene to Live Coding."
+    if dj_mode is False:
+        obs.switchScene('Live Coding')
+        msg = f"@{message.author.name} switched the scene to Live Coding."
+
+    else:
+        msg = f"@{message.author.name} we're currently in DJ mode. Scene switching is disabled."
+    
     await twitch_bot.say(message.channel, msg)
 
 
